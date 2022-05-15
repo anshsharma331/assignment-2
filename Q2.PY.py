fd51ee4ce39eeb1d4903bae39fd0059459531941
anshsharma331 <104381021+anshsharma331@users.noreply.github.com>
2022-05-15 22:16:05 +0530

    Add files via upload

---
+++ Q2.PY.py
@@ -0,0 +1,8 @@
+Type "help", "copyright", "credits" or "license()" for more information.
+name = "ANSH"
+SID = "21102089"
+CGPA = "9.0
+department_name = "CIVIL"
+
+
+print("Hey,", name, "Here!\nMy SID is", SID,"\nI am from", department_name, "department and my CGPA is", CGPA )
